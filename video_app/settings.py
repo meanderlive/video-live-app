73539ddb9d44fb4b26580f5cdde027ce331ae6a9
dev.mspl <developer.mspl@gmail.com>
2024-06-27 16:03:03 +0530

    new code

--- video_app/settings.py
+++ video_app/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
