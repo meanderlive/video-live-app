75d8e54ceba99c2be06535c947ee270f6d55fcc4
dev.mspl <developer.mspl@gmail.com>
2024-06-27 16:38:04 +0530

    update

--- video_app/settings.py
+++ video_app/settings.py
@@ -117,12 +117,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR /'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = 'login'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR /'static']
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# LOGIN_REDIRECT_URL = '/'
+# LOGIN_URL = 'login'
 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
