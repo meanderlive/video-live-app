34e2cf2cfd129be6fb24ed0269e38834ae03ca86
dev.mspl <developer.mspl@gmail.com>
2024-08-13 18:13:02 +0530

    updated code

--- stream/urls.py
+++ stream/urls.py
@@ -13,13 +13,13 @@ from django.urls import path
 app_name = "stream"
 
 urlpatterns = [
-    path('',GeneralVideoListView.as_view(), name="video-list"),
+    # path('',GeneralVideoListView.as_view(), name="video-list"),
     path('video/<int:pk>/', VideoDetailView.as_view(), name="video-detail"),
     path('video/<int:pk>/update/', VideoUpdateView.as_view(), name="video-update"),
     path('video/<int:pk>/delete/', VideoDeleteView.as_view(), name="video-delete"),
     path('user/<str:username>', UserVideoListView.as_view(), name="user-videos"),
     path('video/new/',VideoCreateView.as_view(), name="video-create"),
     path('search',views.search,name="search"),
-    path('home/',views.home,name='home')
+    path('',views.home,name='home')
 ]
 
